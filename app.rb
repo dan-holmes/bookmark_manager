30b8400012758d5cd21800b854c9f4b4fd592d51
Tim Castillo-Gill <timgillviola@gmail.com>
2020-10-20 15:19:54 +0100

    Added pg Gem, and the connection with the database

--- app.rb
+++ app.rb
@@ -1,7 +1,22 @@
 require "sinatra/base"
 require "./lib/bookmark"
+require "pg"
 
 class BookmarkManager < Sinatra::Base
+
+  begin
+    con = PG.connect :dbname => 'bookmark_manager', :user => 'TimGill'
+    puts con.server_version
+    puts '1234'
+
+  rescue PG::Error => e
+    puts e.message
+
+  ensure
+    con.close if con
+
+  end
+
   get "/" do
     "This is a bookmark manager."
   end
--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,5 +1,6 @@
 class Bookmark
   def self.all
-    ["facebook.com", "google.com"]
+    # ["facebook.com", "google.com"]
+    con.exec "SELECT * FROM bookmarks"
   end
 end
